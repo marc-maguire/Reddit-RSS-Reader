f0c1855a00a504b6a7bc4cd4fbee3ba90c14c57d
Marc Maguire <maguire.marc@gmail.com>
2018-02-24 17:49:18 -0500

    updated print statement in ContentViewControllerr to resolve string interpolation warning

--- RedditRSSReader/View Controllers/ContentViewController.swift
+++ RedditRSSReader/View Controllers/ContentViewController.swift
@@ -17,7 +17,7 @@ class ContentViewController: UIViewController, WKNavigationDelegate {
 	private var contentURL: String? {
 		didSet {
 			guard let urlString = self.contentURL, urlString != self.currentlyLoadedURL, let url = URL(string: urlString) else {
-				print("failed to make url from urlString: \(self.contentURL), or URL was already loaded.")
+				print("failed to make url from urlString: \(String(describing: self.contentURL)), or URL was already loaded.")
 				return }
 			self.currentlyLoadedURL = urlString
 			let urlRequest = URLRequest(url: url)
